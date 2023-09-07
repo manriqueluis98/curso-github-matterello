7ba87897ff437dc5eedb831636bf8e942a6b5d49
Luis <manriqueleo98@gmail.com>
2023-09-06 20:57:31 -0500

    Il Matterello Inicializado

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const reservaBtn = document.getElementById("reservaBtn");
+
+  function handleClick() {
+
+
+
+    window.alert("Gracias por reservar");
+  }
+
+  reservaBtn.addEventListener("click", handleClick);
+});
